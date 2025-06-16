9c17e0dcab87dcd8f0c67bd74fbec94e76a38d08
Iago Ferreira <iago-ferreira@outlook.com>
2025-06-16 00:31:39 -0300

    feat: generating zip file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use clap::Parser;
 use std::env;
+use std::fs::File;
+use std::io::{Read, Seek, Write};
+use std::path::{Path};
 use std::process::Command;
+use anyhow::Context;
+use walkdir::{DirEntry, WalkDir};
+use zip::write::SimpleFileOptions;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -18,7 +24,7 @@ struct Args {
     pub output_dir: String,
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
 
     println!("URI {}", args.uri);
@@ -26,10 +32,12 @@ fn main() {
     println!("collections {:?}", args.collections);
 
     let current_dir = env::current_dir().unwrap().display().to_string();
-    for collection in args.collections {
-        let output_dir = format!("{}/backup/{}.json", current_dir.clone(), collection.clone(),);
+    let output_dir = format!("{}/backup/", current_dir.clone());
+    let output_zip = format!("{}/backup.zip", current_dir.clone());
+    for collection in &args.collections {
+        let output_file = format!("{}{}.json", output_dir.clone(), collection.clone());
 
-        println!("{output_dir}");
+        println!("{output_file}");
 
         let output = Command::new("mongoexport")
             .arg("--uri")
@@ -39,13 +47,66 @@ fn main() {
             .arg("--collection")
             .arg(collection.clone())
             .arg("--out")
-            .arg(output_dir)
+            .arg(output_file.clone())
             .arg("--jsonFormat")
             .arg("canonical")
             .arg("--type")
             .arg("json")
             .output()
             .expect("failed to execute process");
+
         println!("{:?}", output);
     }
+
+    if !Path::new(&output_dir).is_dir() {
+        panic!("Output directory does not exist");
+    }
+    let output_zip_path = Path::new(&output_zip);
+    let file = File::create(output_zip_path).unwrap();
+    let walkdir = WalkDir::new(output_dir.clone());
+    let it = walkdir.into_iter();
+
+    zip_dir(&mut it.filter_map(|e| e.ok()), output_dir.clone().as_ref(), file, zip::CompressionMethod::Stored)?;
+    return Ok(())
+}
+
+
+fn zip_dir<T>(
+    it: &mut dyn Iterator<Item = DirEntry>,
+    prefix: &Path,
+    writer: T,
+    method: zip::CompressionMethod,
+) -> anyhow::Result<()>
+where
+    T: Write + Seek,
+{
+    let mut zip = zip::ZipWriter::new(writer);
+    let options = SimpleFileOptions::default()
+        .compression_method(method)
+        .unix_permissions(0o755);
+
+    let prefix = Path::new(prefix);
+    let mut buffer = Vec::new();
+    for entry in it {
+        let path = entry.path();
+        let name = path.strip_prefix(prefix)?;
+        let path_as_string = name
+            .to_str()
+            .map(str::to_owned)
+            .with_context(|| format!("{name:?} Is a Non UTF-8 Path"))?;
+        if path.is_file() {
+            println!("adding file {path:?} as {name:?} ...");
+            zip.start_file(path_as_string, options)?;
+            let mut f = File::open(path)?;
+
+            f.read_to_end(&mut buffer)?;
+            zip.write_all(&buffer)?;
+            buffer.clear();
+        } else if !name.as_os_str().is_empty() {
+            println!("adding dir {path_as_string:?} as {name:?} ...");
+            zip.add_directory(path_as_string, options)?;
+        }
+    }
+    zip.finish()?;
+    Ok(())
 }

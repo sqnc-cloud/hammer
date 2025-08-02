94a9f534e495e512bae5c1cf666e98bb43dad273
Iago Ferreira <iago-ferreira@outlook.com>
2025-08-02 20:40:58 -0300

    feat: added upload flag to commands

--- cmd/backup/command.go
+++ cmd/backup/command.go
@@ -8,21 +8,37 @@ import (
 
 var (
 	connectionString string
+	upload             bool
 )
 
 var BackupCmd = &cobra.Command{
 	Use:   "backup",
 	Short: "Extract your backup to a zipfile or uploads to aws",
 	Long:  ``,
-	Run: func(cmd *cobra.Command, args []string) {
+	RunE: func(cmd *cobra.Command, args []string) error {
 		uri := "mongodb://localhost:27017"
 		database := "hammer"
-		internal.ExportCollections(uri, database)
+		backupFile, err := internal.ExportCollections(uri, database)
+
+		if err != nil {
+			return fmt.Errorf("error to create a backup %v", err)
+		}
+
+		if upload {
+			awsConfig, err := internal.LoadAWSConfigFromEnv()
+			if err != nil {
+				return err
+			}
+
+			return internal.UploadToS3(backupFile, awsConfig.Bucket, awsConfig.AccessKeyID, awsConfig.SecretAccessKey, awsConfig.Region)
+		}
+		return nil
 	},
 }
 
 func init() {
 	BackupCmd.Flags().StringVarP(&connectionString, "db", "d", "", "Database connection string")
+	BackupCmd.Flags().BoolVarP(&upload, "upload", "u", false, "Upload to aws")
 
 	if err := BackupCmd.MarkFlagRequired("db"); err != nil {
 		fmt.Println("Database connection string not defined")

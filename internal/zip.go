be70e3677c073f3db9a8df771e12d92d210c050a
Iago Ferreira <iago-ferreira@outlook.com>
2025-08-03 11:56:59 -0300

    chore: removing useless comments

--- internal/zip.go
+++ internal/zip.go
@@ -7,7 +7,6 @@ import (
 	"path/filepath"
 )
 
-// ZipFolder zips the content of the source folder to the destination zip file.
 func ZipFolder(sourceFolder, destZipFile string) error {
 	zipFile, err := os.Create(destZipFile)
 	if err != nil {
@@ -63,3 +62,43 @@ func ZipFolder(sourceFolder, destZipFile string) error {
 	return nil
 }
 
+func Unzip(src, dest string) error {
+	r, err := zip.OpenReader(src)
+	if err != nil {
+		return err
+	}
+	defer r.Close()
+
+	for _, f := range r.File {
+		fpath := filepath.Join(dest, f.Name)
+
+		if f.FileInfo().IsDir() {
+			os.MkdirAll(fpath, os.ModePerm)
+			continue
+		}
+
+		if err = os.MkdirAll(filepath.Dir(fpath), os.ModePerm); err != nil {
+			return err
+		}
+
+		outFile, err := os.OpenFile(fpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+		if err != nil {
+			return err
+		}
+
+		rc, err := f.Open()
+		if err != nil {
+			return err
+		}
+
+		_, err = io.Copy(outFile, rc)
+
+		outFile.Close()
+		rc.Close()
+
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
